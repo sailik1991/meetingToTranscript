6ecc8a443f7d6125f1f53b72b653fd6a737d86dc
sailiks <ssengu15@asu.edu>
2016-11-26 19:24:01 -0700

    Adding code to suggest DA sequences for completing meetings to achieve consistency

--- HMMs/trainHMM.py
+++
@@ -1,9 +0,0 @@
-from hmmlearn.hmm import MultinomialHMM
-
-M = 2
-
-if __name__ == "__main__":
-    
-    # Initialize and train an HMM model
-    model = MultimonialHMM( n_components = M, n_iter = 100 )
-    
